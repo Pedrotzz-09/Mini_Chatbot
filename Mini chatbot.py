9be3a882d8182f156d77241eb3405785af7c2bca
Pedrotzz <105949290+Pedrotzz-09@users.noreply.github.com>
2022-11-21 18:38:37 -0300

    Update Mini chatbot.py

--- Mini chatbot.py
+++ Mini chatbot.py
@@ -1,10 +1,11 @@
-nome = input('Qual é o seu nome?')
-print("É um prazer te conhecer{}, seu nome é muito bonito!".format(nome))
-idade = int(input('Bom, qual é a sua idade?'))
-if idade == 20:
-  print('Nossa, você tem a minha idade,{}!'.format(nome))
-elif idade > 20:
-  print('Caramba, sou mais novo que você!')
+name = input('What is your name?')
+print("It is a pleasure to meet you{}, you have a very nice name!".format(name))
+print("I don't have a name yet, but you can call me prototype.001!")
+age = int(input('How old are you?'))
+if age == 20:
+  print('Whoa, you are my age,{}!'.format(name))
+elif age > 20:
+  print('Hey, you are older than me!')
 else:
-  print('Sou mais velho que você!')
-print('Bem, foi legal conversar com você{}, espero que possamos nos falar novamente, até logo!'.format(nome))
+  print('I am older than you!')
+print('Well, it was nice to talk to you{}, I hope we can chat again soon, see ya!'.format(name))
